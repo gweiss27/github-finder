b2a86323a6acbe301eae7651728609315e19ce87
Gregory Weiss <gweiss27@gmail.com>
2020-02-28 23:54:22 -0800

    Bugfix for Git API Key Config Variables

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -15,10 +15,10 @@ let githubClientSecret;
 
 if (process.env.NODE_ENV !== "production") {
     githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
 } else {
     githubClientId = process.env.GITHUB_CLIENT_ID;
-    githubClientSecret = process.env.GITHUB_CLIENT_ID;
+    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
 }
 const GithubState = props => {
     const initialState = {
